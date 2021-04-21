74dac512b6cbaa25566d20d4ac0ce368f02e0f27
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2021-04-21 14:44:38 -0300

    Criado os arquivos de rotas da aplicação e configurado no arquivo App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import {
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
-import { Confirmation } from './src/pages/Confirmation';
+
+import Routes from './src/routes';
 
 export default function App() {
 
@@ -18,6 +19,6 @@ export default function App() {
   return <AppLoading />
 
   return (
-    <Confirmation />
+    <Routes />
   )
 }

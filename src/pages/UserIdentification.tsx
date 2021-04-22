3c39f38dd9eaae7af4ad972df1c83f0d01bcdc62
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2021-04-22 13:49:13 -0300

    Validação de preenchimento do campo nome.

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -8,7 +8,8 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Platform,
-  Keyboard
+  Keyboard,
+  Alert
 } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 
@@ -40,6 +41,8 @@ export function UserIdentification() {
   }
 
   function handleSubmit() {
+    if(!name)
+      return Alert.alert('Alerta','Me diz como chamar você 🤔')
     navigation.navigate('Confirmation');
   }
 

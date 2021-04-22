dcc9ca4216e126d4d6dd7e53f5514211d33f01c6
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2021-04-22 15:01:05 -0300

    Adicionado funcionalidade de navegação.

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -6,6 +6,7 @@ import {
   FlatList,
   ActivityIndicator
 } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 import api from '../services/api';
 
@@ -45,6 +46,8 @@ export function PlantSelect() {
 
   const [page, setPage] = useState(1);
   const [loadingMore, setLoadingMore] = useState(true);
+
+  const navigation = useNavigation();
   
 
   function handleEnviromentSelected(enviroment: string) {
@@ -85,6 +88,10 @@ export function PlantSelect() {
     setLoadingMore(true);
     setPage(oldValue => oldValue + 1);
     fetchPlants();
+  }
+
+  function handlePlantSelect(plant: PlantProps) {
+    navigation.navigate('PlantSave', { plant });
   }
 
   useEffect(() => {
@@ -143,7 +150,7 @@ export function PlantSelect() {
           data={filteredPlants}
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-            <PlantCardPrimary data={item} />
+            <PlantCardPrimary data={item} onPress={() => handlePlantSelect(item)} />
           )}
           showsVerticalScrollIndicator={false}
           numColumns={2}

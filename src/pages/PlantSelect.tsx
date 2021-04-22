c88662a4088834bd31bd5cbd5bca1efd34412663
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2021-04-22 13:29:52 -0300

    Ajuste e configuração do componente Load.

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,7 +3,8 @@ import {
   View,
   Text,
   StyleSheet,
-  FlatList
+  FlatList,
+  ActivityIndicator
 } from 'react-native';
 
 import api from '../services/api';
@@ -11,6 +12,7 @@ import api from '../services/api';
 import { Header } from '../components/Header';
 import { EnviromentButton } from '../components/EnviromentButton';
 import { PlantCardPrimary } from '../components/PlantCardPrimart';
+import { Load } from '../components/Load';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -39,6 +41,11 @@ export function PlantSelect() {
   const [plants, setPlants] = useState<PlantProps[]>([]);
   const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
   const [enviromentSelected, setEnviromentSelected] = useState('all');
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(true);
+  const [loadedAll, setLoadedAll] = useState(false);
 
   function handleEnviromentSelected(enviroment: string) {
     setEnviromentSelected(enviroment);
@@ -51,6 +58,33 @@ export function PlantSelect() {
     );
 
     setFilteredPlants(filtered);
+  }
+
+  async function fetchPlants() {
+    const { data } = await api.get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+    if (!data)
+      return setLoading(true);
+
+    if (page > 1) {
+      setPlants(oldValue => [...oldValue, ...data]);
+      setFilteredPlants(oldValue => [...oldValue, ...data]);
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1)
+      return;
+
+    setLoadingMore(true);
+    setPage(oldValue => oldValue + 1);
+    fetchPlants();
   }
 
   useEffect(() => {
@@ -69,14 +103,11 @@ export function PlantSelect() {
   }, [])
 
   useEffect(() => {
-    async function fetchPlants() {
-      const { data } = await api.get('plants?_sort=name&_order=asc');
-      setPlants(data);
-    }
-
     fetchPlants();
   }, [])
 
+  if (loading)
+    return <Load />
   return (
     <View style={styles.container}>
 
@@ -113,13 +144,25 @@ export function PlantSelect() {
             <PlantCardPrimary data={item} />
           )}
           showsVerticalScrollIndicator={false}
-          numColumns={2} />
+          numColumns={2}
+          onEndReachedThreshold={0.1}
+          onEndReached={({ distanceFromEnd }) => 
+            handleFetchMore(distanceFromEnd)
+          }
+          ListFooterComponent={
+            loadingMore 
+            ? <ActivityIndicator color={colors.green_dark} />
+            : <></>
+
+          } />
 
       </View>
 
     </View>
   )
 }
+
+// #astronautas - Código
 
 const styles = StyleSheet.create({
   container: {

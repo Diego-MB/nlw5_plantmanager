adf11708a0bf9d04c1562a1a5150714f4c8c6e30
Diego Maroldi Barreiros <diegomaroldi@gmail.com>
2021-04-23 10:21:09 -0300

    Escutando as notificações.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 
 import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
 
@@ -14,6 +16,15 @@ export default function App() {
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps
+      }
+    )
+
+  }, [])
 
   if(!fontsLoaded)
   return <AppLoading />
